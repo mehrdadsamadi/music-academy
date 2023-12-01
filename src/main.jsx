b31d09a7fd5ac6f4d5bc92936a57b73434ed287c
mehrdadsamadi <samadimehrdad49@gmail.com>
2023-12-01 19:01:58 +0330

    some change

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { Provider } from "react-redux"
+import { RouterProvider } from "react-router-dom"
+
+import { router } from "./routes"
+import { store } from "./redux/store"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
